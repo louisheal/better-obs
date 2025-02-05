a2806f7cdfba2fc2000f71f6354901440c5c6c47
Samuel Braun <sam@webry.com>
2025-02-05 18:13:39 +0100

    Fix import path for MainLayout in App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import MainPage from "@/components/MainPage.vue"
-import MainLayout from "./layouts/MainLayout.vue"
+import MainLayout from "@/layouts/MainLayout.vue"
 import { TooltipProvider } from "#/tooltip"
 </script>
 
