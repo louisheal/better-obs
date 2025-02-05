f653e3adb51e0e2250e1db7ef8efc546b0e83595
Samuel Braun <sam@webry.com>
2025-02-05 18:12:56 +0100

    Added layout & Tooltip-(Provider)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import { Button } from "#/button"
+import MainPage from "@/components/MainPage.vue"
+import MainLayout from "./layouts/MainLayout.vue"
+import { TooltipProvider } from "#/tooltip"
 </script>
 
 <template>
-   <div>
-      <Button> Hi </Button>
-   </div>
+   <TooltipProvider>
+      <MainLayout>
+         <MainPage />
+      </MainLayout>
+   </TooltipProvider>
 </template>
---
+++ frontend/src/components/MainPage.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { Button } from "#/button"
+import { Tooltip, TooltipContent, TooltipTrigger } from "#/tooltip"
+</script>
+
+<template>
+   <div>
+      <Tooltip>
+         <TooltipTrigger as-child>
+            <Button>Hii</Button>
+         </TooltipTrigger>
+         <TooltipContent>
+            <p>Add to library</p>
+         </TooltipContent>
+      </Tooltip>
+   </div>
+</template>
+
+<style scoped></style>
